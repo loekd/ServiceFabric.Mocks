8bac183034bc9927427e8d2b4b2a4f44ea447611
Nick George <me@izzmo.com>
2017-10-06 13:31:31 -0500

    Added spy properties to the MockTransaction to allow for peeking in with unit testing.

--- src/ServiceFabric.Mocks/MockTransaction.cs
+++ src/ServiceFabric.Mocks/MockTransaction.cs
@@ -4,25 +4,35 @@ using Microsoft.ServiceFabric.Data;
 namespace ServiceFabric.Mocks
 {  
     /// <summary>
-   /// A sequence of operations performed as a single logical unit of work.
-   /// </summary>
+    /// A sequence of operations performed as a single logical unit of work.
+    /// </summary>
     public class MockTransaction : ITransaction
     {
         public long CommitSequenceNumber => 0L;
+
+        public bool IsCommitted { get; private set; }
+
+        public bool IsAborted { get; private set; }
+
+        public bool IsCompleted => IsCommitted || IsAborted;
 
         public long TransactionId => 0L;
 
         public void Abort()
         {
+            IsAborted = true;
         }
 
         public Task CommitAsync()
         {
+            IsCommitted = true;
             return Task.FromResult(true);
         }
 
         public void Dispose()
         {
+            Dispose();
+            IsAborted = true;
         }
 
         public Task<long> GetVisibilitySequenceNumberAsync()
