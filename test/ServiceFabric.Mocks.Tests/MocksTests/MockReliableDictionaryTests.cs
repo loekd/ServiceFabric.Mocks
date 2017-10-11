2faae105ae56caed34e40b2846fc2b35edc25906
Loek <admin@admin>
2017-10-11 16:46:26 +0200

    - 2.3.2  - Merged PR by huesie, fix exception type thrown for duplicate key in `MockReliableDictionary`. Added unit tests.

---
+++ test/ServiceFabric.Mocks.Tests/MocksTests/MockReliableDictionaryTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+
+namespace ServiceFabric.Mocks.Tests.MocksTests
+{
+    [TestClass]
+    public class MockReliableDictionaryTests
+    {
+        [TestMethod]
+        public async Task DictionaryAddDuplicateKeyExceptionTypeTest()
+        {
+            const string key = "key";
+            var dictionary = new MockReliableDictionary<string, string>();
+            var tx = new MockTransaction();
+
+            await dictionary.AddAsync(tx, key, "value");
+            await Assert.ThrowsExceptionAsync<ArgumentException>(async () =>
+            {
+                await dictionary.AddAsync(tx, key, "value");
+            });
+        }
+
+        [TestMethod]
+        public async Task DictionaryAddAndRetrieveTest()
+        {
+            const string key = "key";
+            const string value = "value";
+                
+            var dictionary = new MockReliableDictionary<string, string>();
+            var tx = new MockTransaction();
+
+            await dictionary.AddAsync(tx, key, value);
+            var actual = await dictionary.TryGetValueAsync(tx, key);
+
+            Assert.AreEqual(actual.Value, value);
+        }
+    }
+}
