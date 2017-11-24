66b79d6f25d045e271b6fe9e7fde2e779d2761e0
Loek <admin@admin>
2017-11-24 10:00:08 +0100

    - 3.1.1 - Sign assembly

---
+++ test/ServiceFabric.Mocks.Tests/AssemblyStrongNameTests.cs
@@ -0,0 +1,22 @@
+﻿#if RELEASE
+
+using System.Reflection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ServiceFabric.Mocks.Tests
+{
+
+    [TestClass]
+    public class AssemblyStrongNameTests
+    {
+
+        [TestMethod]
+        public void TestAssemblyIdentity()
+        {
+            Assembly main = Assembly.Load("ServiceFabric.Mocks");
+            Assert.IsTrue(main.FullName.Contains("PublicKeyToken=c8a3b3cecf8974ee"));
+        }
+    }
+}
+
+#endif
