927ba2c2f15b45819a1770265718e669aeef4719
Pulsifer, Eli <eli.pulsifer@doverfs.com>
2018-05-20 01:42:48 -0500

    add support for missing actors with same ID but different types

--- src/ServiceFabric.Mocks/MockActorProxyFactory.cs
+++ src/ServiceFabric.Mocks/MockActorProxyFactory.cs
@@ -8,91 +8,95 @@ using System.Collections.Generic;
 
 namespace ServiceFabric.Mocks
 {
-	/// <summary>
-	/// Specifies the interface for the factory that creates proxies for remote communication to the specified Actor.
-	/// </summary>
-	public class MockActorProxyFactory : MockServiceProxyFactory, IActorProxyFactory
-	{
+    /// <summary>
+    /// Specifies the interface for the factory that creates proxies for remote communication to the specified Actor.
+    /// </summary>
+    public class MockActorProxyFactory : MockServiceProxyFactory, IActorProxyFactory
+    {
+        /// <summary>
+        /// Fires when an Actor is missing, set <see cref="MissingActorEventArgs.ActorInstance"/> to resolve.
+        /// </summary>
+        public event EventHandler<MissingActorEventArgs> MissingActor;
 
-		/// <summary>
-		/// Fires when an Actor is missing, set <see cref="MissingActorEventArgs.ActorInstance"/> to resolve.
-		/// </summary>
-		public event EventHandler<MissingActorEventArgs> MissingActor;
+        private readonly ConcurrentDictionary<ActorId, HashSet<IActor>> _actorRegistry = new ConcurrentDictionary<ActorId, HashSet<IActor>>();
 
-		private readonly ConcurrentDictionary<ActorId, HashSet<IActor>> _actorRegistry = new ConcurrentDictionary<ActorId, HashSet<IActor>>();
-		
-		/// <summary>
-		/// Registers an instance of a Actor combined with its Id to be able to return it from 
-		/// <see cref="CreateActorProxy{TActorInterface}(Microsoft.ServiceFabric.Actors.ActorId,string,string,string)"/>
-		/// </summary>
-		/// <param name="actor"></param>
-		public void RegisterActor(IActor actor)
-		{
-			_actorRegistry.AddOrUpdate(
-				actor.GetActorId(),
-				id => new HashSet<IActor>(new[] { actor }),
-				(id, set) =>
-				{
-					if (set.Any(a => a.GetType() == actor.GetType()))
-					{
-						throw new ArgumentException($"There is already an actor of type {actor.GetType()} with the actorId {id}.");
-					}
+        /// <summary>
+        /// Registers an instance of a Actor combined with its Id to be able to return it from 
+        /// <see cref="CreateActorProxy{TActorInterface}(Microsoft.ServiceFabric.Actors.ActorId,string,string,string)"/>
+        /// </summary>
+        /// <param name="actor"></param>
+        public void RegisterActor(IActor actor)
+        {
+            _actorRegistry.AddOrUpdate(
+                actor.GetActorId(),
+                id => new HashSet<IActor>(new[] { actor }),
+                (id, set) =>
+                {
+                    if (set.Any(a => a.GetType() == actor.GetType()))
+                    {
+                        throw new ArgumentException($"There is already an actor of type {actor.GetType()} with the actorId {id}.");
+                    }
 
-					set.Add(actor);
-					return set;
-				});
-		}
+                    set.Add(actor);
+                    return set;
+                });
+        }
 
-		///<inheritdoc />
-		public TActorInterface CreateActorProxy<TActorInterface>(ActorId actorId, string applicationName = null,
-			string serviceName = null, string listenerName = null) where TActorInterface : IActor
-		{
-			if (!_actorRegistry.ContainsKey(actorId))
-			{
-				OnMisingActor(this, actorId, typeof(TActorInterface));
-			}
+        ///<inheritdoc />
+        public TActorInterface CreateActorProxy<TActorInterface>(ActorId actorId, string applicationName = null,
+            string serviceName = null, string listenerName = null)
+            where TActorInterface : IActor
+        {
+            return CreateActorProxy<TActorInterface>(actorId);
+        }
 
-			var set = _actorRegistry[actorId];
-			return set.OfType<TActorInterface>().SingleOrDefault();
-		}
+        ///<inheritdoc />
+        public TActorInterface CreateActorProxy<TActorInterface>(Uri serviceUri, ActorId actorId, string listenerName = null)
+            where TActorInterface : IActor
+        {
+            return CreateActorProxy<TActorInterface>(actorId);
+        }
 
-		///<inheritdoc />
-		public TActorInterface CreateActorProxy<TActorInterface>(Uri serviceUri, ActorId actorId, string listenerName = null)
-			where TActorInterface : IActor
-		{
-			if (!_actorRegistry.ContainsKey(actorId))
-			{
-				OnMisingActor(this, actorId, typeof(TActorInterface));
-			}
+        ///<inheritdoc />
+        public TServiceInterface CreateActorServiceProxy<TServiceInterface>(Uri serviceUri, ActorId actorId, string listenerName = null)
+            where TServiceInterface : IService
+        {
+            var serviceInterface = CreateServiceProxy<TServiceInterface>(serviceUri);
+            return serviceInterface;
+        }
 
-			var set = _actorRegistry[actorId];
-			return set.OfType<TActorInterface>().SingleOrDefault();
-		}
+        ///<inheritdoc />
+        public TServiceInterface CreateActorServiceProxy<TServiceInterface>(Uri serviceUri, long partitionKey, string listenerName = null)
+            where TServiceInterface : IService
+        {
+            var serviceInterface = CreateServiceProxy<TServiceInterface>(serviceUri);
+            return serviceInterface;
+        }
 
-		///<inheritdoc />
-		public TServiceInterface CreateActorServiceProxy<TServiceInterface>(Uri serviceUri, ActorId actorId, string listenerName = null)
-			where TServiceInterface : IService
-		{
-			var serviceInterface = CreateServiceProxy<TServiceInterface>(serviceUri);
-			return serviceInterface;
-		}
+        protected virtual IActor OnMisingActor(object sender, ActorId id, Type actorType)
+        {
+            var args = new MissingActorEventArgs(actorType, id);
+            MissingActor?.Invoke(sender, args);
+            if (args.ActorInstance != null)
+            {
+                RegisterActor(args.ActorInstance);
+                return args.ActorInstance;
+            }
+            return null;
+        }
 
-		///<inheritdoc />
-		public TServiceInterface CreateActorServiceProxy<TServiceInterface>(Uri serviceUri, long partitionKey, string listenerName = null)
-			where TServiceInterface : IService
-		{
-			var serviceInterface = CreateServiceProxy<TServiceInterface>(serviceUri);
-			return serviceInterface;
-		}
+        protected TActorInterface CreateActorProxy<TActorInterface>(ActorId actorId)
+            where TActorInterface : IActor
+        {
+            if (!_actorRegistry.TryGetValue(actorId, out HashSet<IActor> set))
+                return (TActorInterface)OnMisingActor(this, actorId, typeof(TActorInterface));
 
-		protected virtual void OnMisingActor(object sender, ActorId id, Type actorType)
-		{
-			var args = new MissingActorEventArgs(actorType, id);
-			MissingActor?.Invoke(sender, args);
-			if (args.ActorInstance != null)
-			{
-				RegisterActor(args.ActorInstance);
-			}
-		}
-	}
+            var actor = set.OfType<TActorInterface>().SingleOrDefault();
+
+            if (actor != null)
+                return actor;
+
+            return (TActorInterface)OnMisingActor(this, actorId, typeof(TActorInterface));
+        }
+    }
 }
