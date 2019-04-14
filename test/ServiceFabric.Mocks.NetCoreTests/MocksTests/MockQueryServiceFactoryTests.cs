a09e9b10488167791a7e365df4a2d41ae7b6ab0c
Vaclav Haken <vaclav@vaclavkabat.net>
2019-04-14 11:40:55 +0100

    fix candidate for issue #86

---
+++ test/ServiceFabric.Mocks.NetCoreTests/MocksTests/MockQueryServiceFactoryTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Fabric.Health;
+using System.Fabric.Query;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ServiceFabric.Mocks.NetCoreTests.MocksTests
+{
+    [TestClass]
+    public class MockQueryServiceFactoryTests
+    {
+        [TestMethod]
+        public void CreateStatelessServiceInstanceTest()
+        {
+            Assert.IsNotNull(MockQueryServiceFactory.CreateStatelessServiceInstance(new Uri("fabric:/app/service"),
+                "serviceType", "1.0", HealthState.Ok, ServiceStatus.Active));
+        }
+    }
+}
