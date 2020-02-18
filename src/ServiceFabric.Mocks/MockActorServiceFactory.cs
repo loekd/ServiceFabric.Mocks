6198563030b10ca8eab858cef4149325e3232a0a
LoekD <admin@admin.com>
2020-02-18 09:36:24 +0100

    - Allow injection of stateManagerFactory, instead of hard coded value - Adjust test - Updated csproj files

--- src/ServiceFabric.Mocks/MockActorServiceFactory.cs
+++ src/ServiceFabric.Mocks/MockActorServiceFactory.cs
@@ -18,10 +18,14 @@ namespace ServiceFabric.Mocks
         /// <param name="context">Optional Actor ServiceContext. By default, <see cref="MockStatefulServiceContextFactory.Default"/> is used.</param>
         /// <param name="settings">Optional settings. By default, null is used.</param>
         /// <returns></returns>
-        public static MockActorService<TActor> CreateActorServiceForActor<TActor>(Func<ActorService, ActorId, ActorBase> actorFactory = null, IActorStateProvider actorStateProvider = null, StatefulServiceContext context = null, ActorServiceSettings settings = null)
+        public static MockActorService<TActor> CreateActorServiceForActor<TActor>(Func<ActorService, ActorId, ActorBase> actorFactory = null, IActorStateProvider actorStateProvider = null, StatefulServiceContext context = null, ActorServiceSettings settings = null, Func<ActorBase, IActorStateProvider, IActorStateManager> stateManagerFactory = null)
             where TActor : Actor
         {
-            Func<ActorBase, IActorStateProvider, IActorStateManager> stateManagerFactory = (actr, stateProvider) => new MockActorStateManager();
+            if (stateManagerFactory == null)
+            {
+                stateManagerFactory = (actr, stateProvider) => new MockActorStateManager();
+            }
+
             if (actorStateProvider == null)
             {
                 actorStateProvider = new MockActorStateProvider();
@@ -33,54 +37,47 @@ namespace ServiceFabric.Mocks
             return svc;
         }
 
-		private static Dictionary<ActorId, IActorStateManager> stateManagerMap = new Dictionary<ActorId, IActorStateManager>();
-		/// <summary>
-		/// Creates a new <see cref="TActorService"/> (which is an <see cref="ActorService"/>) using <see cref="MockActorStateManager"/> and <see cref="MockStatefulServiceContextFactory.Default"/>
-		/// which returns instances of <see cref="TActor"/> using the optionally provided <paramref name="actorFactory"/>, <paramref name="actorStateProvider"/> and <paramref name="settings"/>.
-		/// </summary>
-		/// <typeparam name="TActor"></typeparam>
-		/// <typeparam name="TActorService"></typeparam>
-		/// <param name="actorFactory">Optional Actor factory. By default, null is used.</param>
-		/// <param name="actorStateProvider">Optional Actor State Provider. By default, <see cref="MockActorStateProvider"/> is used.</param>
-		/// <param name="context">Optional Actor ServiceContext. By default, <see cref="MockStatefulServiceContextFactory.Default"/> is used.</param>
-		/// <param name="settings">Optional settings. By default, null is used.</param>
-		/// <returns></returns>
-		public static TActorService CreateCustomActorServiceForActor<TActorService, TActor>(Func<ActorService, ActorId, ActorBase> actorFactory = null, IActorStateProvider actorStateProvider = null, StatefulServiceContext context = null, ActorServiceSettings settings = null)
-			where TActor : Actor
-			where TActorService : ActorService
-		{
-			Func<ActorBase, IActorStateProvider, IActorStateManager> stateManagerFactory =
-			(actr, stateProvider) =>
-			{
-				if (!stateManagerMap.TryGetValue(actr.Id, out var actorStateManager))
-				{
-					actorStateManager = new MockActorStateManager();
-					stateManagerMap[actr.Id] = actorStateManager;
-				}
-				return actorStateManager;
-			};
+        /// <summary>
+        /// Creates a new <see cref="TActorService"/> (which is an <see cref="ActorService"/>) using <see cref="MockActorStateManager"/> and <see cref="MockStatefulServiceContextFactory.Default"/>
+        /// which returns instances of <see cref="TActor"/> using the optionally provided <paramref name="actorFactory"/>, <paramref name="actorStateProvider"/> and <paramref name="settings"/>.
+        /// </summary>
+        /// <typeparam name="TActor"></typeparam>
+        /// <typeparam name="TActorService"></typeparam>
+        /// <param name="actorFactory">Optional Actor factory. By default, null is used.</param>
+        /// <param name="actorStateProvider">Optional Actor State Provider. By default, <see cref="MockActorStateProvider"/> is used.</param>
+        /// <param name="context">Optional Actor ServiceContext. By default, <see cref="MockStatefulServiceContextFactory.Default"/> is used.</param>
+        /// <param name="settings">Optional settings. By default, null is used.</param>
+        /// <returns></returns>
+        public static TActorService CreateCustomActorServiceForActor<TActorService, TActor>(Func<ActorService, ActorId, ActorBase> actorFactory = null, IActorStateProvider actorStateProvider = null, StatefulServiceContext context = null, ActorServiceSettings settings = null, Func<ActorBase, IActorStateProvider, IActorStateManager> stateManagerFactory = null)
+            where TActor : Actor
+            where TActorService : ActorService
+        {
+            if (stateManagerFactory == null)
+            {
+                stateManagerFactory = (actr, stateProvider) => new MockActorStateManager();
+            }
 
-			if (actorStateProvider == null)
-			{
-				actorStateProvider = new MockActorStateProvider();
-				actorStateProvider.Initialize(ActorTypeInformation.Get(typeof(TActor)));
-			}
+            if (actorStateProvider == null)
+            {
+                actorStateProvider = new MockActorStateProvider();
+                actorStateProvider.Initialize(ActorTypeInformation.Get(typeof(TActor)));
+            }
 
-			context = context ?? MockStatefulServiceContextFactory.Default;
+            context = context ?? MockStatefulServiceContextFactory.Default;
 
-			var ctor = typeof(TActorService).GetConstructor(Constants.InstancePublicNonPublic, null,
-				new []
-				{
-					typeof(StatefulServiceContext),
-					typeof(ActorTypeInformation),
-					typeof(Func<ActorService, ActorId, ActorBase>),
-					typeof(Func<ActorBase, IActorStateProvider, IActorStateManager>),
-					typeof(IActorStateProvider),
-					typeof(ActorServiceSettings),
-				}, null);
-			if (ctor == null) throw new InvalidOperationException("This helper only works for an ActorService with a default constructor. Please create your own instance of {TActorService}.");
-			var svc = ctor.Invoke(new object[] { context, ActorTypeInformation.Get(typeof(TActor)), actorFactory, stateManagerFactory, actorStateProvider, settings});
-			return (TActorService) svc;
-		}
-	}
+            var ctor = typeof(TActorService).GetConstructor(Constants.InstancePublicNonPublic, null,
+                new[]
+                {
+                    typeof(StatefulServiceContext),
+                    typeof(ActorTypeInformation),
+                    typeof(Func<ActorService, ActorId, ActorBase>),
+                    typeof(Func<ActorBase, IActorStateProvider, IActorStateManager>),
+                    typeof(IActorStateProvider),
+                    typeof(ActorServiceSettings),
+                }, null);
+            if (ctor == null) throw new InvalidOperationException("This helper only works for an ActorService with a default constructor. Please create your own instance of {TActorService}.");
+            var svc = ctor.Invoke(new object[] { context, ActorTypeInformation.Get(typeof(TActor)), actorFactory, stateManagerFactory, actorStateProvider, settings });
+            return (TActorService)svc;
+        }
+    }
 }
