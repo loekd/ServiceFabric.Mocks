dded2a3b0e178855d6dad9f67a8fc4f837fdd460
LoekD <admin@loekd.com>
2020-09-30 22:12:59 +0200

    first lines of FabricClient mocking code

---
+++ src/ServiceFabric.Mocks/ReflectionHelpers.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+using static ServiceFabric.Mocks.Constants;
+
+namespace ServiceFabric.Mocks
+{
+    internal static class ReflectionHelpers
+    {
+        public static T CreateInstance<T>()
+        {
+            return (T)Activator.CreateInstance(typeof(T), BindingFlags.Instance | BindingFlags.NonPublic, null, null, CultureInfo.CurrentCulture);
+        }
+
+        public static T GetPropertyValue<T>(this object input, string propertyName)
+        {
+            return (T)GetProperty(input, propertyName).GetValue(input);
+        }
+
+        public static void SetPropertyValue(this object input, string propertyName, object value)
+        {
+           GetProperty(input, propertyName).SetValue(input, value);
+        }
+
+        private static PropertyInfo GetProperty(object input, string propertyName)
+        {
+            return input?.GetType().GetProperty(propertyName, InstancePublicNonPublic);
+        }
+    }
+}
---
+++ test/ServiceFabric.Mocks.NetCoreTests/FabricClientTests/MockFabricClientTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Fabric;
+using System.Text;
+using static ServiceFabric.Mocks.MockFabricClient;
+
+namespace ServiceFabric.Mocks.NetCoreTests.FabricClientTests
+{
+    [TestClass]
+    public class MockFabricClientTests
+    {
+        [TestMethod]
+        public void Test()
+        {
+            var client = new ServiceManagementClient();
+            client.AddResolvedServicePartition(new Uri("test://mocks", UriKind.Absolute), MockQueryPartitionFactory.CreateIntPartitonInfo(), new[] { MockQueryPartitionFactory.CreateResolvedServiceEndpoint("test://mocks") });
+        }
+    }
+}
