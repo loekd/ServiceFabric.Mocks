1ec2b91f2f971737def2b24687e3b5fc6a661375
LoekD <loekd@hotmail.com>
2022-10-13 11:34:11 +0200

    pwsh core

--- ExtractPrivateKey.ps1
+++ ExtractPrivateKey.ps1
@@ -5,8 +5,14 @@
 
 [string] $pfxFilePath = ".\src\ServiceFabric.Mocks\ServiceFabric.Mocks.pfx"
 [string] $snkFilePath = [IO.Path]::GetFileNameWithoutExtension($pfxFilePath) + ".snk";
-[byte[]] $certificateContent = Get-Content $pfxFilePath -Encoding Byte;
+[byte[]] $certificateContent = Get-Content $pfxFilePath -AsByteStream;
 $exportable = [Security.Cryptography.X509Certificates.X509KeyStorageFlags]::Exportable
 $certificate = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2($certificateContent, $pfxPwd, $exportable)
-$certificateContent = ([Security.Cryptography.RSACryptoServiceProvider]$certificate.PrivateKey).ExportCspBlob($true)
+
+$rsaPrivateKey = [System.Security.Cryptography.X509Certificates.RSACertificateExtensions]::GetRSAPrivateKey($certificate)
+$rsaParameters = $rsaPrivateKey.ExportParameters($true)   
+$csp = New-Object Security.Cryptography.RSACryptoServiceProvider
+$csp.ImportParameters($rsaParameters)
+$certificateContent = $csp.ExportCspBlob($true)
+
 [IO.File]::WriteAllBytes([IO.Path]::Combine([IO.Path]::GetDirectoryName($pfxFilePath), $snkFilePath), $certificateContent)
